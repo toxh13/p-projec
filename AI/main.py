c494171c424090161a172a80561c17011ae08648
toxh13 <144295745+toxh13@users.noreply.github.com>
2024-12-07 01:27:40 +0900

    Update main.py

--- AI/main.py
+++ AI/main.py
@@ -5,8 +5,3 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
-
-# 예시로 /recommendation 엔드포인트 추가
-@app.get("/recommendation")
-def get_recommendation():
-    return {"recommendation": "상품 추천 정보"}
