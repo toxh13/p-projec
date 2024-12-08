3eb3cd6fa63d238daa137448bdee75aa984a5755
Trevorjun <150532881+Trevorjun@users.noreply.github.com>
2024-12-08 18:54:27 +0900

    Update main.py

--- AI/main.py
+++ AI/main.py
@@ -54,6 +54,6 @@ def recommend(request: RecommendationRequest):
     return recommendations.to_dict(orient='records')
 
 # FastAPI 서버 실행
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
